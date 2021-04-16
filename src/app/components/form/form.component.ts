0bb08870d324bf8d462cf3ec84d16698c32dd160
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-04-16 12:05:05 +0100

    Primitive HTML form implemented. Needs to be adjusted to use ngModel template directive.

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { formItem } from '../../models/formItem'
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/models/formItem.ts
@@ -0,0 +1,10 @@
+export class formItem {
+    amount: number;
+    currSelected: string;
+    currToConvTo: string;
+    constructor() {
+    this.amount = 50
+    this.currSelected = 'GBP'
+    this.currToConvTo = 'USD'
+    }
+}
